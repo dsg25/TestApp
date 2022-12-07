3c6903f2612eddb8391bdba51020a6ae869905ec
Kocha <dsg25@bk.ru>
2022-12-07 06:01:21 -0800

    test

---
+++ TestApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestApp
+//
+//  Created by Kocha on 07.12.2022.
+//  Copyright © 2022 DarkGrp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, MUD")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
